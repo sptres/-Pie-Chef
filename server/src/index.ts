f9beb23fdc5f6559d4a1a4faedbf8c7bba565087
sptres <shawnpark2397@gmail.com>
2024-06-19 18:02:16 -0700

    routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ mongoose
     console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB:', error);
   });
 
 app.use('/api', recipeRoutes);
--- server/src/routes/recipe.ts
+++ server/src/routes/recipe.ts
@@ -1,18 +1,40 @@
-import { Router } from "express";
+import { Router } from 'express';
 import Recipe from '../models/recipe';
 
 const router = Router();
 
 router.post('/recipes', async (req, res) => {
-    const {title, image, time, ingredients, difficultyLevel } = req.body;
-    try {
-        const newRecipe = new Recipe({
-            title,
-            image,
-            time,
-            ingredients,
-            difficultyLevel,
-        });
-        await 
+  const { title, image, time, ingredients, difficultyLevel } = req.body;
+  try {
+    const newRecipe = new Recipe({
+      title,
+      image,
+      time,
+      ingredients,
+      difficultyLevel,
+    });
+    await newRecipe.save();
+    res.status(201).json(newRecipe);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(400).json({ message: 'Unknown error' });
     }
-})
+  }
+});
+
+router.get('/recipes', async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.status(200).json(recipes);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(400).json({ message: 'Unknown error' });
+    }
+  }
+});
+
+export default router;
